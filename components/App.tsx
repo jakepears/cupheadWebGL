6b7a608ea3dcf8fe75c66fc873d1c13c7516a96a
DaCodeWiz <jake.pearson.1694@gmail.com>
2023-10-17 12:10:27 -0700

    changed to cuphead

--- components/App.tsx
+++ components/App.tsx
@@ -5,10 +5,10 @@ import { Unity, useUnityContext } from 'react-unity-webgl';
 
 export default function App() {
 	const { unityProvider, isLoaded, loadingProgression } = useUnityContext({
-		loaderUrl: '/Build/pokemonWebGl.loader.js',
-		dataUrl: '/Build/pokemonWebGL.data',
-		frameworkUrl: '/Build/pokemonWebGL.framework.js',
-		codeUrl: '/Build/pokemonWebGL.wasm',
+		loaderUrl: '/Build/cupWeb.loader.js',
+		dataUrl: '/Build/cupWeb.data',
+		frameworkUrl: '/Build/cupWeb.framework.js',
+		codeUrl: '/Build/cupWeb.wasm',
 	});
 
 	const loadingPercentage = Math.round(loadingProgression * 100);
@@ -17,7 +17,7 @@ export default function App() {
 			{isLoaded === false && (
 				// We'll conditionally render the loading overlay if the Unity
 				// Application is not loaded.
-				<div className='loading-overlay'>
+				<div className='top-0 left-0 '>
 					<p>Loading... ({loadingPercentage}%)</p>
 				</div>
 			)}
